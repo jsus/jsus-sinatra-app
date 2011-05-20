4108ff995426acb2b5bf310e3eae42198015b701
Mark Abramov <markizko@gmail.com>
2011-05-20 18:00:54 +0400

    Examples for /include/ directive of edge jsus

--- app.rb
+++ app.rb
@@ -5,10 +5,11 @@ require 'jsus'
 require 'sinatra/base'
 
 Jsus::Middleware.settings = {
-  :cache        => true,
-  :cache_path   => File.expand_path("../public/javascripts/jsus/require", __FILE__),
-  :packages_dir => File.expand_path("../vendor/js", __FILE__),
-  :cache_pool   => false
+  :cache         => true,
+  :cache_path    => File.expand_path("../public/javascripts/jsus/require", __FILE__),
+  :packages_dir  => File.expand_path("../vendor/js", __FILE__),
+  :cache_pool    => false,
+  :includes_root => File.expand_path("../vendor/js", __FILE__)
 }
 
 class JsusApplication < Sinatra::Base
@@ -19,7 +20,8 @@ class JsusApplication < Sinatra::Base
       200, {"Content-Type" => "text/plain"},
       "Ping! Use jsus urls to see some magic.\n\n"    <<
       "E.g.: /javascripts/jsus/require/CKEditor.js\n" <<
-      "/javascripts/jsus/require/CKEditor~CKEditor:core.config~CKEditor:core.command.js\n" <<
+      "/javascripts/jsus/include/CKEditor.js\n"       <<
+      "/javascripts/jsus/require/CKEditor~CKEditor:core.config~CKEditor:core.command.js\n"             <<
       "/javascripts/jsus/require/CKEditor:plugins.div.plugin+CKEditor:plugins.domiterator.plugin.js\n" <<
       "Or even: /javascripts/jsus/require/**:*undo*.js\n"
     ]
